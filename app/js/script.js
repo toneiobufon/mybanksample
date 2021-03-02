f5b58acd7f937cf670b09df154bdd183aeecfcb2
toneiojimz <ajimeneza77@gmail.com>
2021-03-01 20:31:43 -0800

    added animation to hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,21 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){ //close hamburger menu
         header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+        
     }
     else {
-        header.classList.add('open');
+        header.classList.add('open'); // open hamburger menu
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
     }
 });
